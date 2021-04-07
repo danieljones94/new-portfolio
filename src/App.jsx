b474012df341552a7ab167626f34d6508e145a8d
Daniel Jones <danielmjones1010@gmail.com>
2021-04-07 17:03:52 +0100

    bootstrap navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavBar from "./components/navBar/NavBar";
 
 const App = () => {
-  return <div className="App">My Portfolio</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
