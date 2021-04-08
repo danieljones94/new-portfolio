b8cf4ceb1c497356882fde4f992f9d087a16f192
Daniel Jones <danielmjones1010@gmail.com>
2021-04-08 18:35:56 +0100

    skills section

---
+++ src/data/skills.js
@@ -0,0 +1,59 @@
+import AngularJS from "../assets/images/Skills/AngularJS.jpg";
+import Angular from "../assets/images/Skills/Angular.jpg";
+import Azure from "../assets/images/Skills/Azure.jpg";
+import Bootstrap from "../assets/images/Skills/Bootstrap.jpg";
+import CSharp from "../assets/images/Skills/CSharp.jpg";
+import CSS from "../assets/images/Skills/CSS.jpg";
+import Dotnet from "../assets/images/Skills/Dotnet.jpg";
+import GIT from "../assets/images/Skills/GIT.jpg";
+import HTML from "../assets/images/Skills/HTML.jpg";
+import JavaScript from "../assets/images/Skills/JavaScript.jpg";
+import MongoDB from "../assets/images/Skills/MongoDB.jpg";
+import Postgresql from "../assets/images/Skills/Postgresql.jpg";
+import React from "../assets/images/Skills/React.jpg";
+import ReactRouter from "../assets/images/Skills/ReactRouter.jpg";
+import Redux from "../assets/images/Skills/Redux.jpg";
+import SASS from "../assets/images/Skills/SASS.jpg";
+import SemanticUI from "../assets/images/Skills/SemanticUI.jpg";
+import SQL from "../assets/images/Skills/SQL.jpg";
+import SSMS from "../assets/images/Skills/SSMS.jpg";
+import StoryBook from "../assets/images/Skills/StoryBook.jpg";
+import TypeScript from "../assets/images/Skills/TypeScript.jpg";
+
+export const skills = {
+  frontEnd: [
+    { skill: "HTML5", imgAltText: "HTML5", imgSrc: HTML },
+    { skill: "CSS", imgAltText: "CSS", imgSrc: CSS },
+    { skill: "SASS", imgAltText: "SASS", imgSrc: SASS },
+    { skill: "Bootstrap", imgAltText: "Bootstrap", imgSrc: Bootstrap },
+    { skill: "Semantic UI", imgAltText: "Semantic UI", imgSrc: SemanticUI },
+    { skill: "Storybook", imgAltText: "Storybook", imgSrc: StoryBook },
+  ],
+  backEnd: [
+    {
+      skill: "SSMS",
+      imgAltText: "SSMS",
+      imgSrc: SSMS,
+    },
+    { skill: "Postgresql", imgAltText: "PostgreSQL", imgSrc: Postgresql },
+    { skill: "MongDB", imgAltText: "MongoDB", imgSrc: MongoDB },
+  ],
+  versionControl: [
+    { skill: "GIT", imgAltText: "GIT", imgSrc: GIT },
+    { skill: "Azure", imgAltText: "Azure", imgSrc: Azure },
+  ],
+  frameworksAndLibraries: [
+    { skill: "React", imgAltText: "React", imgSrc: React },
+    { skill: "React Router", imgAltText: "React Router", imgSrc: ReactRouter },
+    { skill: "Redux", imgAltText: "Redux", imgSrc: Redux },
+    { skill: "Angular", imgAltText: "Angular", imgSrc: Angular },
+    { skill: "AngularJS", imgAltText: "AngularJS", imgSrc: AngularJS },
+    { skill: "Dotnet", imgAltText: "Dotnet", imgSrc: Dotnet },
+  ],
+  languages: [
+    { skill: "JavaScript", imgAltText: "JavaScript", imgSrc: JavaScript },
+    { skill: "TypeScript", imgAltText: "Typescript", imgSrc: TypeScript },
+    { skill: "C#", imgAltText: "C#", imgSrc: CSharp },
+    { skill: "SQL", imgAltText: "SQL", imgSrc: SQL },
+  ],
+};
